9fbcb0084e9a763b3ec3c453f16119638a4ba1df
acorrea-B <aljyque95@gmail.com>
2021-07-27 21:14:43 -0500

    Se crea los casos de prueba para superadmin y el operador

--- back-end/apps/users/tests.py
+++ back-end/apps/users/tests.py
@@ -1,3 +1,57 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+class UsersTestCase(TestCase):
+    def setUp(self):
+        self.admin = {"identification" : "1234",
+                      "password" : "pass1234567",
+                      "first_name":"a",
+                      "last_name":"b",
+                      "user_type":1,
+                      "phoneNumber":"1256756"
+                    } 
+        self.operator = {"first_name":"a",
+                         "last_name":"b",
+                         "identification":"1231298",
+                         "password":"er",
+                         "user_type":3,
+                         "is_staff":True,
+                         "phoneNumber":"213123123",
+                         "email":"asdas@commerce.co"
+                        }
+        self.customer = {"first_name":"pepito",
+                              "last_name":"perez",
+                              "identification":"3245645",
+                              "password":"er",
+                              "user_type":2,
+                              "phoneNumber":"2131250678",
+                              "email":"asdas@example.com"
+                            }
+
+    def test_create_super_user(self):
+        user = get_user_model().objects.create_superuser(**self.admin)
+        user.save()
+        
+        user_create = get_user_model().\
+                      objects.get(self.admin.get("identification"))
+        
+        self.assertTrue(user_create)
+        self.assertTrue(user_create.admin)
+        self.assertTrue(user_create.staff)
+        self.assertTrue(user_create.is_active)
+        self.assertTrue(user_create.is_superuser)
+
+    
+    def test_create_operator(self):
+        user = get_user_model().objects.create_user( **self.operator )
+        user.save()
+        user_create = get_user_model().\
+                      objects.get(self.operator.get("identification"))
+        
+        self.assertTrue(user_create)
+        self.assertFalse(user_create.admin)
+        self.assertTrue(user_create.staff)
+        self.assertTrue(user_create.is_active)
+        self.assertTrue(user_create.is_superuser)
+    
+    
