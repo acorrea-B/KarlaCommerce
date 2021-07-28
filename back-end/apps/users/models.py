d8ddb013d48fd3012d0f28c8001f03a7e148da8e
acorrea-B <aljyque95@gmail.com>
2021-07-27 21:34:04 -0500

    Se crea el modelo para la gestion de usuarios, tanto operadores como clientes

--- back-end/apps/users/models.py
+++ back-end/apps/users/models.py
@@ -1,3 +1,85 @@
+from django.contrib.auth.models import AbstractUser,BaseUserManager
 from django.db import models
 
-# Create your models here.
+class UserManager(BaseUserManager):
+    """
+        La clase UserManager se usa para sobre escribir los metodos
+        usados por django.contrib.auth para crear los diferentes tipos 
+        de usuarios.
+
+        En este caso se sobre escribe el metodo create_user para que registre a 
+        los usuarios de tipo operator y costumer, el metodo create_superuser se 
+        sobre escribe para que pueda crear un super usuario con los campos definidos 
+        en el modelo.  
+
+    """
+
+    def create_user(self, identification, user_type, first_name, 
+                    last_name, phoneNumber, email, password=None,
+                    is_admin=False, is_staff=False, is_active=True):
+        if not identification:
+            raise ValueError("User must have an identification")
+        if not password:
+            raise ValueError("User must have a password")
+        
+        if(user_type == 3): is_staff = True
+
+        user = self.model(
+            identification=identification,
+            first_name = first_name,
+            last_name = last_name,
+            phoneNumber = phoneNumber,
+            email = email
+        )
+        user.user_type = user_type
+        user.set_password(password)  
+        user.admin = is_admin
+        user.staff = is_staff
+        user.active = is_active
+
+        user.save(using=self._db)
+        return user
+        
+    def create_superuser(self, identification, user_type, 
+                         first_name, last_name, password,
+                         phoneNumber = None, **extra_fields):
+        if not identification:
+            raise ValueError("User must have an identification")
+        if not password:
+            raise ValueError("User must have a password")
+
+
+        user = self.model(
+            identification=identification,
+            first_name = first_name,
+            last_name = last_name,
+            phoneNumber = phoneNumber,
+        )
+        user.user_type = user_type
+        user.set_password(password)
+        user.staff = True
+        user.is_superuser = True
+        user.admin =True
+        user.active = True
+        user.save(using=self._db)
+        return user
+
+class User(AbstractUser):
+    #tipos de usuarios pemitidos en el sistema
+    USER_TYPE_CHOICES = (
+      (1, 'admin'),
+      (2, 'costumer'),
+      (3, 'operator'),
+    )
+    username=None
+    #se extiende el modelo de usuarios de django con los campos que se requieren para el comercio
+    phoneNumber=models.CharField(unique=True, max_length=15)
+    identification=models.CharField(max_length=12, unique=True)
+    email=models.CharField(unique=True, max_length=50, blank=True)
+    user_type = models.PositiveSmallIntegerField(choices=USER_TYPE_CHOICES)
+  
+    #cambio de autenticacion, por autenticacion por medio del numero de identificacion y password
+    USERNAME_FIELD = 'identification'
+    REQUIRED_FIELDS = ['user_type']    
+    
+    objects = UserManager()
