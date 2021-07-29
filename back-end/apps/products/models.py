a18c6dd8e9b7edfac0e7e7f886bedb0dd28e7deb
acorrea-B <aljyque95@gmail.com>
2021-07-29 15:22:20 -0500

    Se define el modelo para los productos, con sus respectivos campos.

--- back-end/apps/products/models.py
+++ back-end/apps/products/models.py
@@ -1,3 +1,15 @@
+import uuid
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    # Identificador unico del producto
+    id = models.UUIDField(primary_key = True, default = uuid.uuid4, editable = False)
+    # Nombre del producto
+    name = models.CharField(max_length = 200)
+    # Valor del producto
+    value = models.DecimalField(max_digits = 10, decimal_places = 2)
+    # Breve descripcion del producto
+    description = models.TextField(blank = True)
+    # Nombre de la imagen almacenada en S3
+    image = models.CharField(max_length = 200)
+
